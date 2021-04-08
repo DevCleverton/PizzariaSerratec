03e61015c44a6019fe2f68970c2e7e08c0661000
Afonso Nunes Rodrigues <fonsor8@gmail.com>
2021-04-08 11:25:24 -0300

    fazendo mundaças

--- js/pizzaria.js
+++ js/pizzaria.js
@@ -7,9 +7,13 @@ Dinheiro
  */
 
 function formaDePagamento() {
-    pagamento = parseInt(prompt("Digi"));
+    pagamento = parseInt(
+        prompt(
+            "Digite a forma de pagamento desejada: \n 1 - Cartão de Crédito ou Débito\n 2 - Pix\n 3 - PicPay\n 4 - Dinheiro"
+        )
+    );
     switch (pagamento) {
-        case value:
+        case 1:
             break;
 
         default:
