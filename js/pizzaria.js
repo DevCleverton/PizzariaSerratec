69d8e39992d6844d1836254114daecb66fd6e40e
Afonso Nunes Rodrigues <fonsor8@gmail.com>
2021-04-08 11:44:00 -0300

    alterações

--- js/pizzaria.js
+++ js/pizzaria.js
@@ -14,6 +14,22 @@ function formaDePagamento() {
     );
     switch (pagamento) {
         case 1:
+            alert(
+                "Ok, a forma de pagamento escolhida foi Cartão de Crédito ou Débito"
+            );
+            break;
+        case 2:
+            alert("Ok, a forma de pagamento escolhida foi Pix");
+            break;
+        case 3:
+            alert("Ok, a forma de pagamento escolhida foi PicPay");
+            break;
+        case 4:
+            alert("Ok, a forma de pagamento escolhida foi Dinheiro");
+            let valorParaTroco = parseInt(
+                prompt("Digite o valor para troco: ")
+            );
+            valorParaLevar = valorParaTroco - valorTotal;
             break;
 
         default:
